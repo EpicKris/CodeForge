47da5aef2ab7a387d846d466c22030847d8f2b66
Kristian Matthews <kristian.matthews@apple.com>
2014-05-30 15:07:00 +0100

    Updated cron schedule

--- source/application/controllers/cron.php
+++ source/application/controllers/cron.php
@@ -19,6 +19,76 @@ class Cron extends CI_Controller {
 	 * @return void
 	 */
 	public function index() {
+		$this->yearly();
+	}
+
+	/**
+	 * Index for yearly cron controller.
+	 * 
+	 * Maps to the following URL: example.com/cron/yearly
+	 * 
+	 * Run once a year at midnight on the morning of January 1.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function yearly() {
+		$this->monthly();
+	}
+
+	/**
+	 * Index for monthly cron controller.
+	 * 
+	 * Maps to the following URL: example.com/cron/monthly
+	 * 
+	 * Run once a month at midnight on the morning of the first day of the month.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function monthly() {
+		$this->weekly();
+	}
+
+	/**
+	 * Index for weekly cron controller.
+	 * 
+	 * Maps to the following URL: example.com/cron/weekly
+	 * 
+	 * Run once a week at midnight on Sunday morning.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function weekly() {
+		$this->daily();
+	}
+
+	/**
+	 * Index for daily cron controller.
+	 * 
+	 * Maps to the following URL: example.com/cron/daily
+	 * 
+	 * Run once a day at midnight.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function daily() {
+		$this->hourly();
+	}
+
+	/**
+	 * Index for hourly cron controller.
+	 * 
+	 * Maps to the following URL: example.com/cron/hourly
+	 * 
+	 * Run once an hour at the beginning of the hour.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function hourly() {
 		
 	}
 }

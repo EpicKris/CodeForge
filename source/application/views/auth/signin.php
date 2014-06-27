c8b9b68a754dd5039d00cfd079abbdc1cf1233ac
Kristian Matthews <kristian.matthews@me.com>
2014-06-27 13:40:30 +0100

    Added scripts

--- source/application/views/auth/signin.php
+++ source/application/views/auth/signin.php
@@ -49,6 +49,17 @@
 		<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script src="/resources/js/bootstrap.min.js"></script>
 		<script src="/resources/js/codeforge.min.js"></script>
+		<?php
+			if (isset($scripts)) {
+				if (is_array($scripts)) {
+					foreach ($scripts as $script) {
+						echo '<script src="' . $script . '"></script>';
+					}
+				} else {
+					echo '<script src="' . $scripts . '"></script>';
+				}
+			}
+		?>
 
 	</body>
 
